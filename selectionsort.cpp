7c7619529f1f62c99837b4e03638c67fd8472ff3
Blacksmith1111 <150678839+Blacksmith1111@users.noreply.github.com>
2024-04-25 15:23:18 +0300

    Add files via upload
    SelectionSort version

---
+++ selectionsort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "sort.hpp"
+#include <limits>
+
+int main(){
+	std::vector<long> vec;
+	int n{0};
+	std::string buffer;
+	std::cin >> n;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignoring the endline character
+	getline(std::cin, buffer);
+	vector_creating(vec, buffer);
+	SelectionSort(vec);
+	for(int i = 0; i < vec.size(); i++){
+		std::cout << vec[i] << " ";
+	}
+}
+
