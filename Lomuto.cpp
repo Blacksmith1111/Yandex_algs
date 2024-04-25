e64e1fc9473d6dcf98060cd64566a994407f9b85
Blacksmith1111 <150678839+Blacksmith1111@users.noreply.github.com>
2024-04-25 13:20:34 +0300

    Update Lomuto.cpp
    rearranged

--- Lomuto.cpp
+++ Lomuto.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <limits>
-
-template <typename T>
-std::vector<T>& Lomuto(std::vector<T>& vec);
-template <typename T>
-std::vector<T>& vector_creating(std::vector<T>& vec, std::string& buffer);
-
+#include "sort.hpp"
 
 int main(){
 	std::vector<long> vec;
@@ -22,35 +17,3 @@ int main(){
 	}
 	return 0;
 }
-
-
-template <typename T>
-std::vector<T>& Lomuto(std::vector<T>& vec){
-	//vec[0] is a pivotal point
-	int index = 1;
-	for(int i = 2; i < vec.size(); i++){
-		if(vec[i] <= vec[0]){
-			std::swap(vec[index], vec[i]);
-			index++;
-		}
-	}
-	std::swap(vec[0], vec[index - 1]);
-	return vec;
-}
-
-template <typename T>
-std::vector<T>& vector_creating(std::vector<T>& vec, std::string& buffer){
-	std::string temp{};
-	for(auto&& str : buffer){
-		if(str == ' '){
-			vec.push_back(std::atol(temp.c_str()));
-			temp.clear();
-			continue;
-		}
-		temp += str;
-	}
-	if(!temp.empty()){
-		vec.push_back(std::atol(temp.c_str()));
-	}
-	return vec;
-}	
